2c9f3fbe9a97af0db7c9c2d2fe2493bae3b186c9
Tohir Ravshanov <ravshanovtohir11@gmail.com>
2026-09-21 12:21:39 +0500

    fix comapre bug

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,17 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import { LoginRequestDto } from './dto';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { CreateAuthDto, LoginRequestDto } from './dto';
 import { PrismaService } from '@prisma';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
 
   async findAll() {
     return `This action returns all auth`;
   }
 
+  async validate(login: string) {
+    const staff = await this.prisma.staff.findUnique(
+      {
+        where: {
+          login: login
+        },
+        select: {
+          id: true,
+          name: true,
+          login: true,
+          password: true
+        }
+      }
+    )
+
+    if(!staff) {
+      throw new NotFoundException('Пользователь не существует!')
+    }
+
+    return {
+      id: staff.id,
+      name: staff.name,
+      login: staff.login,
+      password: staff.password
+    }
+  }
+
   async login(data: LoginRequestDto) {
-    void data;
-    throw new Error('Login is not implemented yet.');
+    const staff = await this.validate(data.login)
+
+    if(!staff) {
+      throw new NotFoundException('User with this login not found!')
+    }
+
+    const isMatch = await bcrypt.compare(data.password, staff.password);
+
+    if (!isMatch) {
+      throw new UnauthorizedException('Недействительные учетные данные!');
+    }
   }
 }
